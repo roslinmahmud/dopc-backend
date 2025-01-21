8da827e18df909ef51a12cb24cffce912137ceb7
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2025-01-22 01:53:19 +0200

    updated endpoints

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"Wolt 2025 Backend Engineering Internship Assignment"}
 
 app.include_router(endpoints.router, prefix="/api/v1")
